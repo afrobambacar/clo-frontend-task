96b7f73642134a8de4461b6d41a6edcdc99a168a
Lee Jae-Cheol <jclee.dev@gmail.com>
2025-04-17 03:33:39 +0900

    Fix: test after build

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run preview',
+    command: 'npm run build && npm run preview',
     url: 'http://localhost:3001',
     reuseExistingServer: !process.env.CI,
   },
